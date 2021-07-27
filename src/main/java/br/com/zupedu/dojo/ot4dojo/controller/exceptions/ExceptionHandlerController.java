85c9bf6de3e42d6ee2215d32c4883ef545b4c641
Mauricio Montanha Junior <mauriciomontanhajunior@gmail.com>
2021-07-27 17:25:34 -0300

    piloto Mauricio Montanha co-piloto *

--- src/main/java/br/com/zupedu/dojo/ot4dojo/controller/exceptions/ExceptionHandlerController.java
+++ src/main/java/br/com/zupedu/dojo/ot4dojo/controller/exceptions/ExceptionHandlerController.java
@@ -1,17 +1,24 @@
 package br.com.zupedu.dojo.ot4dojo.controller.exceptions;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ExceptionHandlerController {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErroPadrao> erro(MethodArgumentNotValidException exception){
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        List<Erros> erros = fieldErrors.stream().map(f -> {
+            return new Erros(f.getDefaultMessage(), f.getField());
+        }).collect(Collectors.toList());
+        return ResponseEntity.badRequest().body(new ErroPadrao(erros));
 
     }
 
